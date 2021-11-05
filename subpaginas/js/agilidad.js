6329be44d82bdd2d964690b01cf220f17233efa5
Anferoid <92281204+Anferoid@users.noreply.github.com>
2021-11-05 00:06:34 -0500

    Initial commit

---
+++ subpaginas/js/agilidad.js
@@ -0,0 +1,28 @@
+document.getElementById("player").addEventListener("mouseover",sumarPuntos);
+
+puntos = 0;
+tiempo = 25;
+necesarios = 30;
+function sumarPuntos(){
+    puntos++;
+    document.getElementById("puntos").innerHTML = "Puntos: <b>" + puntos + "/" + necesarios + "  </b>";
+    randNum = Math.round(Math.random()*350);
+    randNum2 = Math.round(Math.random()*750);
+    document.getElementById("player").style.marginTop = randNum + "px";
+    document.getElementById("player").style.marginLeft = randNum2 + "px";
+    if (puntos == 30) {
+        alert("Ganaste");
+    }
+}
+
+function restarTiempo() {
+    tiempo--;
+    document.getElementById("tiempo").innerHTML = "&nbsp;&nbsp;&nbsp;Tiempo: " + tiempo;
+    if (tiempo == 0) {
+        alert("Perdiste");
+        tiempo = 0;
+        puntos = 0;
+    }
+}
+
+setInterval(restarTiempo,1000);
